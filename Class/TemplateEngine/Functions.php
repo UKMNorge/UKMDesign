40e1f26629fd1e0fe13d11caf83d2c6611dbe50c
Marius Mandal <mariusmandal@gmail.com>
2020-02-15 20:57:08 +0100

    Flyttet UKMAsset til UKMapi

--- Class/TemplateEngine/Functions.php
+++ Class/TemplateEngine/Functions.php
@@ -5,12 +5,4 @@ namespace UKMNorge\Design\TemplateEngine;
 use UKMNorge\Twig\Definitions\Functions as FunctionDefinitions;
 
 class Functions extends FunctionDefinitions {
-
-    public function UKMasset($path)
-	{
-		if ( defined('UKM_HOSTNAME') && UKM_HOSTNAME == 'ukm.dev' ) {
-			return '//ukm.dev/wp-content/themes/UKMDesign/_GRAFIKK_UKM_NO/'. $path;
-		}
-		return '//grafikk.ukm.no/UKMresponsive/'. $path;
-	}
 }
